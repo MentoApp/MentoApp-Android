d1a3b6b3f30c9e819e4958a3ca89acf1df0061c1
최현정 <hjchoi.dev@gmail.com>
2025-01-29 13:44:15 +0900

    Initial commit

---
+++ app/src/main/java/com/mentoapp/dotorit/presentation/ui/theme/Color.kt
@@ -0,0 +1,38 @@
+package com.mentoapp.dotorit.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Neutral color
+
+val Black = Color(0xFF000000)
+val Neutral900 = Color(0xFF212121)
+val Neutral800 = Color(0xFF424242)
+val Neutral700 = Color(0xFF616161)
+val Neutral600 = Color(0xFF757575)
+val Neutral500 = Color(0xFF9E9E9E)
+val Neutral400 = Color(0xFFBDBDBD)
+val Neutral300 = Color(0xFFE0E0E0)
+val Neutral200 = Color(0xFFEEEEEE)
+val Neutral100 = Color(0xFFF5F5F5)
+val Neutral50 = Color(0xFFFAFAFA)
+val White = Color(0xFFFFFFFF)
+
+// Primary (Brand color)
+
+val Orange900 = Color(0xFFFF6A00)
+val Orange300 = Color(0xFFFFD24C)
+val Orange60 = Color(0xFFFBEFE3)
+val Orange50 = Color(0xFFFFF8E0)
+
+// Status color
+
+val StatusError = Color(0xFFFF1F00)
+val StatusConfirmed = Color(0xFF0095FF)
---
+++ app/src/main/java/com/mentoapp/dotorit/presentation/ui/theme/Type.kt
@@ -0,0 +1,113 @@
+package com.mentoapp.dotorit.presentation.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.mentoapp.dotorit.R
+
+val PretendardBold = FontFamily(Font(R.font.pretendard_bold, FontWeight.Bold))
+val PretendardMedium = FontFamily(Font(R.font.pretendard_medium, FontWeight.Medium))
+val PretendardRegular = FontFamily(Font(R.font.pretendard_regular, FontWeight.Normal))
+val PretendardSemiBold = FontFamily(Font(R.font.pretendard_semibold, FontWeight.SemiBold))
+
+@Stable
+class DotoritTypography(
+    headlineBold1: TextStyle,
+    headlineBold2: TextStyle,
+    headlineBold3: TextStyle,
+    headlineBold4: TextStyle,
+    bodyMedium1: TextStyle,
+    bodyMedium2: TextStyle
+) {
+    var headlineBold1: TextStyle by mutableStateOf(headlineBold1)
+        private set
+    var headlineBold2: TextStyle by mutableStateOf(headlineBold2)
+        private set
+    var headlineBold3: TextStyle by mutableStateOf(headlineBold3)
+        private set
+    var headlineBold4: TextStyle by mutableStateOf(headlineBold4)
+        private set
+    var bodyMedium1: TextStyle by mutableStateOf(bodyMedium1)
+        private set
+    var bodyMedium2: TextStyle by mutableStateOf(bodyMedium2)
+        private set
+
+    fun update(other: DotoritTypography) {
+        headlineBold1 = other.headlineBold1
+        headlineBold2 = other.headlineBold2
+        headlineBold3 = other.headlineBold3
+        headlineBold4 = other.headlineBold4
+        bodyMedium1 = other.bodyMedium1
+        bodyMedium2 = other.bodyMedium2
+    }
+
+    fun copy(
+        headlineBold1: TextStyle = this.headlineBold1,
+        headlineBold2: TextStyle = this.headlineBold2,
+        headlineBold3: TextStyle = this.headlineBold3,
+        headlineBold4: TextStyle = this.headlineBold4,
+        bodyMedium1: TextStyle = this.bodyMedium1,
+        bodyMedium2: TextStyle = this.bodyMedium2
+    ): DotoritTypography = DotoritTypography(
+        headlineBold1,
+        headlineBold2,
+        headlineBold3,
+        headlineBold4,
+        bodyMedium1,
+        bodyMedium2
+    )
+}
+
+fun calculateLetterSpacing(fontSize: TextUnit, percentage: Float): TextUnit {
+    return (fontSize.value * (percentage / 100)).sp
+}
+
+@Composable
+fun DotoritTypography(): DotoritTypography {
+    return DotoritTypography(
+        headlineBold1 = TextStyle(
+            fontFamily = PretendardBold,
+            fontSize = 24.sp,
+            lineHeight = 31.2.sp,
+            letterSpacing = calculateLetterSpacing(24.sp, -2.5f)
+        ),
+        headlineBold2 = TextStyle(
+            fontFamily = PretendardBold,
+            fontSize = 20.sp,
+            lineHeight = 31.2.sp,
+            letterSpacing = calculateLetterSpacing(20.sp, -2.5f)
+        ),
+        headlineBold3 = TextStyle(
+            fontFamily = PretendardBold,
+            fontSize = 18.sp,
+            lineHeight = 28.sp,
+            letterSpacing = calculateLetterSpacing(18.sp, -2.5f)
+        ),
+        headlineBold4 = TextStyle(
+            fontFamily = PretendardBold,
+            fontSize = 14.sp,
+            lineHeight = 23.4.sp,
+            letterSpacing = calculateLetterSpacing(14.sp, -2.5f)
+        ),
+        bodyMedium1 = TextStyle(
+            fontFamily = PretendardRegular,
+            fontSize = 14.sp,
+            lineHeight = 19.6.sp,
+            letterSpacing = calculateLetterSpacing(14.sp, -2.5f)
+        ),
+        bodyMedium2 = TextStyle(
+            fontFamily = PretendardRegular,
+            fontSize = 12.sp,
+            lineHeight = 16.8.sp,
+            letterSpacing = calculateLetterSpacing(12.sp, -2.5f)
+        )
+    )
+}
