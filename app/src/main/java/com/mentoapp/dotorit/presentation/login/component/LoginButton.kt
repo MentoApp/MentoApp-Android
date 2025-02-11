b9f3c8ada3d5e4035583d4973cb24fd28a1c3d85
최현정 <hjchoi.dev@gmail.com>
2025-02-11 16:02:11 +0900

    refactor: Move LoginButton

--- app/src/main/java/com/mentoapp/dotorit/presentation/login/component/LoginButton.kt
+++ app/src/main/java/com/mentoapp/dotorit/presentation/login/component/LoginButton.kt
@@ -1,4 +1,4 @@
-package com.mentoapp.dotorit.presentation.ui.component
+package com.mentoapp.dotorit.presentation.login.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
