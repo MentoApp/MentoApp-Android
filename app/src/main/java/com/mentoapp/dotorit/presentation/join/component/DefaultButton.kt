365065303c45fe3cb2804dd432bfca5fd2c5f6cc
최현정 <hjchoi.dev@gmail.com>
2025-02-27 20:23:00 +0900

    DOTORIT-32 feat: Add DefaultButton

---
+++ app/src/main/java/com/mentoapp/dotorit/presentation/join/component/DefaultButton.kt
@@ -0,0 +1,57 @@
+package com.mentoapp.dotorit.presentation.join.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mentoapp.dotorit.presentation.ui.theme.DotoritTheme
+import com.mentoapp.dotorit.presentation.ui.theme.Neutral400
+import com.mentoapp.dotorit.presentation.ui.theme.Neutral900
+import com.mentoapp.dotorit.presentation.ui.theme.White
+import com.mentoapp.dotorit.presentation.ui.theme.dotoritTypography
+
+@Composable
+fun DefaultButton(
+    buttonText: String,
+    enabled: Boolean = true,
+    onClick: (Boolean) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = { onClick(enabled) },
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Neutral900,
+            contentColor = Neutral900,
+            disabledContainerColor = Neutral400,
+            disabledContentColor = Neutral400
+        ),
+        shape = RoundedCornerShape(8.dp),
+        modifier = modifier
+            .fillMaxWidth(),
+        contentPadding = PaddingValues(0.dp)
+    ) {
+        Text(
+            modifier = modifier
+                .padding(horizontal = 70.dp, vertical = 15.dp),
+            text = buttonText,
+            color = White,
+            style = dotoritTypography().bodyMedium1
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoginButtonPreview() {
+    DotoritTheme {
+        DefaultButton("다음")
+    }
+}
